595f8cd93b8c5f34fa4fe0094d64e579aa58ba32
刘宇 <liuyu@liuyutekiMacBook-Air.local>
2013-09-01 23:33:35 +0800

    ls v1

--- ls.py
+++ ls.py
@@ -1,24 +1,18 @@
 import os
 import sys
 
-def showList(curPath):
+def showList(curPath, showHide = 0):
 	step = 0
 	for i in os.listdir(curPath):
-		if step == 8:
-			print 
-			step = 0
-		else:
-			print "%-15s" %i,
-			step = step + 1
-
-
-
-
+		print "%-15s" %i,
 
 if len(sys.argv) == 1:
 	curPath = os.getcwd()
 	showList(curPath)
+elif sys.argv[1].startswith('-'):
+	option = sys.argv[1][1:]
+	if option == 'a':
+		showHide = 1
 else:
 	curPath = sys.argv[1]
 	showList(curPath)
-
